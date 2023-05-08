cc7b942877112632d5ee5319032a47beced12b94
AlinaCartus <alina.cartus@outlook.de>
2023-05-08 16:53:35 +0200

    testform organization info

--- zukunft-bau-app/src/components/general/TestForm.vue
+++ zukunft-bau-app/src/components/general/TestForm.vue
@@ -1,42 +1,49 @@
 <template>
-    <v-sheet width="300" class="mx-auto">
-      <v-form fast-fail @submit.prevent>
-        <v-text-field
-          v-model="firstName"
-          label="First name"
-          :rules="firstNameRules"
-        ></v-text-field>
-  
-        <v-text-field
-          v-model="lastName"
-          label="Last name"
-          :rules="lastNameRules"
-        ></v-text-field>
-  
-        <v-btn type="submit" block class="mt-2">Submit</v-btn>
-      </v-form>
-    </v-sheet>
+    <v-form fast-fail @submit.prevent>
+        <v-text-field v-model="companyName" label="Name des Unternehmens">
+        </v-text-field>
+        <v-text-field v-model="street" label="Straße und Hausnummer">
+        </v-text-field>
+        <v-text-field v-model="zipcode" label="Postleitzahl">
+        </v-text-field>
+        <v-text-field v-model="city" label="Stadt">
+        </v-text-field>
+        <v-text-field v-model="country" label="Land">
+        </v-text-field>
+        <v-text-field v-model="sites" label="Standorte z.B. Köln, Hamburg, München">
+        </v-text-field>
+    </v-form>   
+    <v-btn type="submit" block class="mt-2" @click="sm_companyinformation">Submit</v-btn>
+    
   </template>
 
-  <script>
-  export default {
-    data: () => ({
-      firstName: '',
-      firstNameRules: [
-        value => {
-          if (value?.length > 3) return true
 
-          return 'First name must be at least 3 characters.'
-        },
-      ],
-      lastName: '123',
-      lastNameRules: [
-        value => {
-          if (/[^0-9]/.test(value)) return true
+<script>
+import { computed, ref } from 'vue'
 
-          return 'Last name can not contain digits.'
-        },
-      ],
-    }),
-  }
+
+export default{
+  setup() {
+    const count=ref(0)
+    const companyName=ref('')
+    const city=ref('köln')
+    const street=ref('')
+    const zipcode=ref('')
+    const country=ref('')
+    const sites=ref(['Köln','Hamburg'])
+    let result={}
+    const number_sites = computed(() => {return sites.value.length})
+   
+    const sm_companyinformation = () => {
+        result=JSON.stringify({"companyName":companyName.value,"city":city.value, "street":street.value, "zipcode":zipcode.value,
+                            "country":country.value, "sites":sites.value, "number_sites":number_sites.value})
+        console.log(result)};
+   
+    return {
+      count, companyName, city, street, zipcode, country, number_sites, sites, result, sm_companyinformation,
+    }
+  },
+}
 </script>
+
+  
