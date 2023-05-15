2a89cb3b7ef7dc4eee7c17d43947de9af154b15e
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2023-05-15 10:50:35 +0200

    delete amplify

--- zukunft-bau-app/src/main.js
+++ zukunft-bau-app/src/main.js
@@ -13,10 +13,6 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins';
 
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
 const app = createApp(App)
 
 registerPlugins(app)
