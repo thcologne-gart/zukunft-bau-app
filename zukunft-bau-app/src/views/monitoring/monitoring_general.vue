a6e62dfaf03acd256e78c276e83cfb20bcfa6c51
AlinaCartus <alina.cartus@outlook.de>
2023-05-22 16:01:44 +0200

    buttons to navigate zero

--- zukunft-bau-app/src/views/monitoring/monitoring_general.vue
+++ zukunft-bau-app/src/views/monitoring/monitoring_general.vue
@@ -1,10 +1,14 @@
 <template>
     <v-container fill-height class="mb-3 mx-2" id="maincontents">
-        <v-row no-gutters class="flex-nowrap flex-column flex-grow-1">Monitoring... Welche Gebäude möchtest du sehen?</v-row>
+        <v-row no-gutters class="flex-nowrap">Monitoring... Welche Gebäude möchtest du sehen?</v-row>
         
-        <v-row no-gutters class="flex-nowrap"><v-btn class='mt-5' @click="$router.push({name:'Monitoring_Site', params:{siteid:sites.name}})">
-    <h2> Standort {{ sites.name }}</h2>
-</v-btn></v-row>
+        <v-row no-gutters class="flex-nowrap">
+            <v-col class="flex-column mx-2" v-for="site in sites" :key="site.id" >
+                <v-btn class='mt-2' @click="$router.push({name:'Monitoring_Site', params:{siteid:site.name}})">
+                    <h2> Standort  {{ site.name }}</h2>
+                </v-btn>
+            </v-col>
+        </v-row>
 
 
     </v-container>
@@ -12,10 +16,10 @@
 </template>
 
 <script>
+import { useGeneralStore } from "@/store/general"
 export default{
 setup(){
-        // das hier dann aus deinem store laden:
-        const sites={name:"Köln",id:"1"}
+        const sites=[ {name:"Köln",id:"1"},{name:"Hamburg",id:"2"}]
     return{sites}}
 }
 </script>
