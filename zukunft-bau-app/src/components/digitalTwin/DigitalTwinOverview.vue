aaafad16ca6c4e4453cf7966d8ae514f050757af
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2023-05-22 15:34:50 +0200

    add digital twins view

---
+++ zukunft-bau-app/src/components/digitalTwin/DigitalTwinOverview.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-container>
+            <div>{{ site.siteName }}</div>
+        </v-container>
+    </div>
+</template>
+
+<script>
+export default{
+    props: {
+        site: Object
+    }
+}
+</script>
--- zukunft-bau-app/src/views/digitaltwins/digitaltwins_general.vue
+++ zukunft-bau-app/src/views/digitaltwins/digitaltwins_general.vue
@@ -1,19 +1,46 @@
 <template>
-    <h1>Digital Twins - General</h1>
-    <p> Wähle einen Standort aus, den du sehen möchtest</p>
-    <div v-for="site in sites" :key="site.id" >
-       <router-link :to="{name:'DigitalTwins_Site', params:{siteid:site.name}}">
-        <h2>{{ site.name }}</h2>
-       </router-link>  
+    <div>
+        <h1>Digital Twins - General</h1>
+        <p> Wähle einen Standort aus, den du sehen möchtest</p>
+        <div v-for="site in sites" :key="site.id" >
+        <router-link :to="{name:'DigitalTwins_Site', params:{siteid:site.name}}">
+            <h2>{{ site.name }}</h2>
+        </router-link>  
+        </div>
+        <v-card class="mx-auto my-16"
+        max-width="85%">
+            <v-toolbar
+                color="white"
+                >
+                <v-toolbar-title style="color: #bc3384">
+                    <span>Digital Twins - Übersicht</span>
+                </v-toolbar-title>
+            </v-toolbar>
+            <v-divider :thickness="4"></v-divider>
+            <div v-for="site in generalStore.loadedSiteInformation" :key="site.siteName">
+                <DigitalTwinOverview :site="site"/>
+            </div>
+        </v-card>
     </div>
 </template> 
 
 <script>
+import { useGeneralStore } from "@/store/general"
+import DigitalTwinOverview from "@/components/digitalTwin/DigitalTwinOverview.vue"
+
 export default{
     setup(){
         // das hier dann aus deinem store laden:
         const sites=[ {name:"Köln",id:"1"},{name:"Hamburg",id:"2"} ]
-    return{sites}}
+    return{sites}},
+    components: {
+        DigitalTwinOverview
+    },
+    computed: {
+        generalStore () {
+            return useGeneralStore()
+        }
+    }
 }
 
 </script>
