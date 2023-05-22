792d26f0c870b9645d1e4cc21f2d0f3ea66926b5
AlinaCartus <alina.cartus@outlook.de>
2023-05-22 17:16:31 +0200

    dd

--- zukunft-bau-app/src/views/monitoring/monitoring_general.vue
+++ zukunft-bau-app/src/views/monitoring/monitoring_general.vue
@@ -1,7 +1,8 @@
 <template>
     <v-container fill-height class="mb-3 mx-2" id="maincontents">
         <v-row no-gutters class="flex-nowrap">Monitoring... Welche Gebäude möchtest du sehen?</v-row>
-        
+        <v-row>{{ loadedSiteInformation }}</v-row>
+        <v-row>{{ siteName }}</v-row>
         <v-row no-gutters class="flex-nowrap">
             <v-col class="flex-column mx-2" v-for="site in sites" :key="site.id" >
                 <v-btn class='mt-2' @click="$router.push({name:'Monitoring_Site', params:{siteid:site.name}})">
@@ -19,13 +20,15 @@
 import { useGeneralStore } from "@/store/general"
 import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
+
 export default{
 setup(){
     const generalStore=useGeneralStore()
     const {loadedSiteInformation}=storeToRefs(generalStore)
+    const siteName=loadedSiteInformation
     
     const sites=[ {name:"Köln",id:"1"},{name:"Hamburg",id:"2"}]
-    return{sites}}
+    return{sites, loadedSiteInformation, siteName}}
 }
 </script>
 
@@ -34,13 +37,3 @@ setup(){
 </style>
 
 
-export default{
-    components:{ BuildingDetails, TestForm2},
-    setup () {
-        const buildingStore= useBuildingStore()
-        const {totalCount,favCount,loading}=storeToRefs(buildingStore)
-        buildingStore.getBuildings()
-        const filter = ref('all')
-        return { buildingStore, filter, totalCount,favCount,loading  }
-    }
-}
