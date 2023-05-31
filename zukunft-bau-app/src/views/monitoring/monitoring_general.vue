1d28d583a71ec7775a6d656f4291b98c41a377f9
AlinaCartus <alina.cartus@outlook.de>
2023-05-31 17:14:57 +0200

    site_general: häuser auflisten start

--- zukunft-bau-app/src/views/monitoring/monitoring_general.vue
+++ zukunft-bau-app/src/views/monitoring/monitoring_general.vue
@@ -1,15 +1,15 @@
 <template>
     <v-container fill-height class="maincontents_monitoring mb-0 mx-2" >
-        <v-row no-gutters class="flex-nowrap mb-0 pb-0" justify="space-around" ><h2>Welche Gebäude möchtest du sehen?</h2>  </v-row>
+        <v-row no-gutters class="flex-nowrap mb-0 pb-0" justify="space-around" ><h2>Welche Liegenschaft möchtest du betrachten?</h2>  </v-row>
         <v-row class="mt-0" justify="space-between">
             <v-card class="mx-auto" :width="400" :height="350" v-for="site,key in loadedSiteInformation" :key="key" >
                 
                     <v-row color="#a3a0a0" justify="center">
-                        <v-img :height='200' class='mt-10 ' src="@/assets/Haus_Lupe.svg"></v-img>
+                        <v-img :height='200' class='mt-10 ' src="@/assets/Hauser_Lupe.svg"></v-img>
                 </v-row>
                 <v-row>
                     <v-btn color="highlight" class='mt-15 mx-5' @click="$router.push({name:'Monitoring_Site', params:{siteid:site.siteName}})">
-                    <h3 > {{ site.siteName }} Standort {{ site.city }}</h3>
+                    <h3 >  Standort {{ site.city }} - {{ site.siteName }}</h3>
                 </v-btn>
                 </v-row>
                   
--- zukunft-bau-app/src/views/monitoring/site/site_general.vue
+++ zukunft-bau-app/src/views/monitoring/site/site_general.vue
@@ -1,9 +1,72 @@
 <template >
-    <v-container id="maincontents">
-site
+    <v-container fill-height class="maincontents_monitoring mb-0 mx-2" >
+        <h2>Deine Häuser</h2>
+        {{ place(loadedSiteInformationWithBuildings)}}
+  <v-card v-for="building,key in loadedSiteInformationWithBuildings" :key="key"
+    class="mx-auto"
+    max-width="344"
+  >
+  <!-- {{ building[site] }} -->
+ 
+    <v-img 
+      src="@/assets/Haus_Lupe.svg"
+      height="200px"
+      class='mt-5'
+    ></v-img>
+
+    <v-card-title>
+      Top western road trips
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange-lighten-2"
+        variant="text"
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+    </v-card-actions>
+  </v-card>
+
+
+        <v-row class="mt-10"></v-row>
     </v-container>
 </template>
+
+<script>
+import { useGeneralStore } from "@/store/general"
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default{
+   
+    setup(){
+        const route= useRoute()
+        var siteInfo=[]
+        const site= route.params.siteid
+        const generalStore=useGeneralStore()
+        const {loadedSiteInformationWithBuildings}=storeToRefs(generalStore)
+        const place = (Company) => {for (let key in Company) {
+            console.log(key, Company[key])
+            if (Company[key] = site)
+                {siteInfo=Company} // wenn site=sitename aus route, dann ist das unser Zielobjekt, wo die Gebäude nun näher ausgewählt werden können
+
+        }}
+       
+        
+        return{place,route,site,loadedSiteInformationWithBuildings}}
+   
+}
+</script>
 
 <style scoped>
 @import "../../../styles/styles-zukunft-bau.css"
 </style>
+
