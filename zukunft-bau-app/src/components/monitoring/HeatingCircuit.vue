4ca576614c6cb186f808863ff692593a2927053f
AlinaCartus <alina.cartus@outlook.de>
2023-06-01 16:27:46 +0200

    heating2

---
+++ zukunft-bau-app/src/components/monitoring/HeatingCircuit.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-card>
+  <Line v-if="loaded" :data="data" :options="options" />
+  <v-card-actions>
+     <v-spacer></v-spacer>
+    <v-btn
+      color="orange-lighten-2"
+      variant="text"
+    >
+      Explore
+    </v-btn>
+    <v-btn
+      :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+      @click="show = !show"
+    ></v-btn>
+  </v-card-actions>
+
+  <v-expand-transition>
+    <div v-show="show">
+      <v-divider></v-divider>
+
+      <v-card-text>
+        Ich bin ein Heizkreis. Du siehst hier meine Vorlauf- und Rücklauftemperatur sowie die Außentemperatur. Den Sollwert meiner Vorlauftemperatur siehst du ebenfalls.
+        Auch wann die Pumpe on/off ist kannst du sehen. Dazu klicke einfach auf den Button "Anforderung Wärme?"
+      </v-card-text>
+    </div>
+  </v-expand-transition>
+  </v-card>
+
+</template>
+<script>
+import {
+Chart as ChartJS,
+CategoryScale,
+LinearScale,
+PointElement,
+LineElement,
+Title,
+Tooltip,
+Legend
+} from 'chart.js';
+import { Line } from 'vue-chartjs';
+import { ref } from 'vue';
+import { useMonitoringStore } from '@/store/monitoring';
+
+ChartJS.register(CategoryScale,LinearScale,PointElement,
+LineElement,Title,Tooltip,Legend)
+
+export default{
+  components: {Line},
+  setup () {
+      const DataStore=useMonitoringStore()
+      DataStore.getData()
+
+      const show=ref(false)
+      const loaded=true
+      const data = {
+                  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                  datasets: [
+                              {
+                              label: 'Data One',
+                              backgroundColor: '#f87979',
+                              data: [40, 39, 10, 40, 39, 80, 40]
+                              }
+                              ]}
+      const options={ responsive: true, maintainAspectRatio: false}
+      //const monitoringStore= useMonitoringStore()
+      //const {totalCount,favCount,loading}=storeToRefs(buildingStore)
+      //buildingStore.getBuildings()
+      //const filter = ref('all')
+      return { show, data, loaded, DataStore}
+  }
+}
+</script>
