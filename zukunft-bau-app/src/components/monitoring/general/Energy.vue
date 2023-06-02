636d19ac45253563a92185b4f9b5b0c0cad9ac31
AlinaCartus <alina.cartus@outlook.de>
2023-06-02 11:49:06 +0200

    added bim image and energy general chart

---
+++ zukunft-bau-app/src/components/monitoring/general/Energy.vue
@@ -0,0 +1,63 @@
+<template>
+<v-card>
+  <Doughnut v-if="loaded" :data="data" :options="options" />
+  <v-card-actions>
+     <v-spacer></v-spacer>
+    <v-btn
+      color="highlight"
+      variant="text"
+    >
+      Explore
+
+    </v-btn>
+    <v-btn
+      :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+      @click="show = !show"
+    ></v-btn>
+  </v-card-actions>
+
+  <v-expand-transition>
+    <div v-show="show">
+      <v-divider></v-divider>
+
+      <v-card-text>
+        Ich bin eine Übersicht zum Energieverbrauch
+      </v-card-text>
+    </div>
+  </v-expand-transition>
+  </v-card>
+
+</template>
+
+<script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+import { ref, computed} from 'vue';
+
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+
+  components: {
+    Doughnut
+  },
+  setup () {
+    const show=ref(false)
+    const loaded=true
+    const data={
+  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+      data: [40, 20, 80, 10]
+    }
+  ]
+}
+    const options={ responsive: true, maintainAspectRatio: false}
+    return {loaded,show, data}
+
+  }
+
+}
+</script>
--- zukunft-bau-app/src/views/monitoring/site/building/building_general.vue
+++ zukunft-bau-app/src/views/monitoring/site/building/building_general.vue
@@ -1,37 +1,51 @@
 <template>
   <v-container fill-height class="mb-3 mx-2" >
-     <h1>Monitoring Overview</h1>
- <v-row
-   no-gutters
-   class="flex-nowrap"
- >
-   <v-col
-     cols="4"
-     class="flex-column"
-   >
-      <p>Hier jetzt erst mal das Standard-Chart für einen Heizkreis</p>
-   </v-col>
-   <v-col
-     cols="8"
-     class="flex-column"
-   >
-      <Heizkreis />
-   </v-col>
- </v-row>
+     <!-- HIER ÜBERSCHRIFT ALS NAVIGATOR NUTZEN WENN KLICK AUF HEIZUNG; DANN ADD HEIZUNG DAHINTER ETC.; ALLES ALS LINKS!-->
+     <v-row><h2>Monitoring {{ building }}</h2></v-row>
+
+     <v-row style="height: 20vh" >
+        <v-img :width="300"
+            aspect-ratio="16/9"
+            cover
+            src="@/assets/BIM.jpeg">
+        </v-img>
+     </v-row>
+
+      <v-row
+        class="flex-nowrap"
+      >
+      <!-- HIER DYNAMISCH GENERAL KOMPONENTEN REINLADEN UND WENN KLICK AUF EINE DER KOMPONENTEN, DANN SPEZIFISCHE KOMPONENTEN EINLADEN-->
+        <v-col
+          cols="4"
+          class="flex-column"
+        >
+            <Energieverbrauch />
+        </v-col>
+        <v-col
+          cols="8"
+          class="flex-column"
+        >
+            <Heizkreis />
+        </v-col>
+      </v-row>
   </v-container>
 
 </template>
 
 <script>
-import Heizkreis from '@/components/monitoring/HeatingCircuit.vue'
+import Heizkreis from '@/components/monitoring/heating/HeatingCircuit.vue'
+import Energieverbrauch from '@/components/monitoring/general/Energy.vue'
+import { useRoute } from 'vue-router';
 export default{
- components:{ Heizkreis },
+ components:{ Energieverbrauch, Heizkreis },
  setup () {
+        const route= useRoute();
+        const building= route.params.buildingid;
      //const monitoringStore= useMonitoringStore()
      //const {totalCount,favCount,loading}=storeToRefs(buildingStore)
      //buildingStore.getBuildings()
      //const filter = ref('all')
-     //return { buildingStore, filter, totalCount,favCount,loading  }
+     return { building }
  }
 }
 </script>
