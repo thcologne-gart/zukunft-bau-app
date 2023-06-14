775d82c5fde1fb6987679fb671c24933d1c296ea
AlinaCartus <alina.cartus@outlook.de>
2023-06-14 13:10:42 +0200

    add images

--- zukunft-bau-app/src/views/monitoring/site/building/building_general.vue
+++ zukunft-bau-app/src/views/monitoring/site/building/building_general.vue
@@ -20,19 +20,31 @@
         >
         <component :is ="component" />
       </v-col
-      >
-      <v-col md="4"
-          class="flex-column"
-          >
-          <h2 class="mb-3"> Funktioniert mein Gebäude? </h2>
+      ></v-row>
 
-<v-btn v-for="element in grundfunktionen" :key="element" class="my-3 mx-2">
-  <v-avatar size="36px" rounded="0" >
-    <v-img height="100%" :src="element[[Object.keys(element)[0]]]"></v-img>
-  </v-avatar>
-  <span class="ml-5">{{ Object.keys(element)[0] }}</span>
-</v-btn>
-        </v-col>
+      <v-row>
+        <h2 class="mb-3"> Funktioniert mein Gebäude?  </h2>
+        <v-row>
+          <v-col md="6"
+          class="flex-column" > <v-img :width="600"
+            aspect-ratio="16/9"
+            cover
+            src="@/assets/Wärme_Haus.svg">
+        </v-img>
+
+          </v-col>
+          <v-col md="4"
+          class="flex-column" >
+            <v-btn v-for="element in grundfunktionen" :key="element" class="my-3 mx-2">
+                <v-avatar size="36px" rounded="0" >
+                  <v-img height="100%" :src="element[[Object.keys(element)[0]]]"></v-img>
+                </v-avatar>
+                <span class="ml-5">{{ Object.keys(element)[0] }}</span>
+              </v-btn>
+
+          </v-col>
+
+        </v-row>
 
       </v-row>
   </v-container>
