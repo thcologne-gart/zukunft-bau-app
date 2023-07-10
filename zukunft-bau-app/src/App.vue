53ef18301b1b56db2fba90321bb048cd870fcd7d
Maximilian Both <maximilian_alexander.both@th-koeln.de>
2023-07-10 11:20:52 +0200

    add styling to auth

--- zukunft-bau-app/src/App.vue
+++ zukunft-bau-app/src/App.vue
@@ -8,12 +8,6 @@
           />
         </div>
       </template>
-      <!--
-      <template v-slot="{ user, signOut }">
-        <h1>Hello {{ user.username }}!</h1>
-        <button @click="signOut">Sign Out</button>
-      </template>
-    -->
     </Authenticator>
       <template v-if="auth.authStatus === 'authenticated'">
         <NavBar />
